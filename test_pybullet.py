d7d0544e95825a71d50a3b77b9cf3c62f5a48584
sergey-rubtsov <sergey.rubtsov81@gmail.com>
2021-05-03 15:55:07 +0300

    Add exported mixamo urdf and test examples

---
+++ test_pybullet.py
@@ -0,0 +1,31 @@
+import pybullet as p
+import time
+import pybullet_data
+import pathlib
+import transformations
+import math
+
+
+# Start pybullet simulation
+p.connect(p.GUI)
+# p.connect(p.DIRECT) # don't render
+
+# load urdf file path
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+# load urdf and set gravity
+p.setGravity(0,0,-10)
+planeId = p.loadURDF("plane.urdf")
+cubeStartPos = [0,0,1]
+# cubeStartOrientation = p.getQuaternionFromEuler([0,-1,1])
+cubeStartOrientation = transformations.quaternion_from_euler(math.pi / 2, 0, 0, 'szyx')
+p.setAdditionalSearchPath(str(pathlib.Path(__file__).parent.absolute()))
+# boxId = p.loadURDF("/urdf/humanoid.urdf", cubeStartPos, cubeStartOrientation)
+boxId = p.loadURDF("/urdf/robot.urdf", cubeStartPos, cubeStartOrientation)
+
+# step through the simluation
+for i in range (10000):
+    p.stepSimulation()
+    time.sleep(1./240.)
+    p.getBodyInfo(1)
+
+p.disconnect()
