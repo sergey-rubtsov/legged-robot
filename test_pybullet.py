9e1830c48c3811659ba7a3f8e5c313135095b063
sergey-rubtsov <sergey.rubtsov81@gmail.com>
2021-05-05 20:41:21 +0300

    Fix robot.urdf, add examples

--- test_pybullet.py
+++ test_pybullet.py
@@ -10,22 +10,22 @@ import math
 p.connect(p.GUI)
 # p.connect(p.DIRECT) # don't render
 
-p.configureDebugVisualizer(p.COV_ENABLE_GUI, 1)
+# p.configureDebugVisualizer(p.COV_ENABLE_GUI, 1)
 p.configureDebugVisualizer(p.COV_ENABLE_KEYBOARD_SHORTCUTS, 1)
 p.configureDebugVisualizer(p.COV_ENABLE_MOUSE_PICKING, 1)
 p.configureDebugVisualizer(p.COV_ENABLE_SHADOWS, 1)
 p.configureDebugVisualizer(p.COV_ENABLE_RENDERING, 1)
-p.configureDebugVisualizer(p.COV_ENABLE_WIREFRAME, 1)
+# p.configureDebugVisualizer(p.COV_ENABLE_WIREFRAME, 1)
 # load urdf file path
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 # load urdf and set gravity
-p.setGravity(0,0,-10)
+p.setGravity(0, 0, -10)
 planeId = p.loadURDF("plane.urdf")
 cubeStartPos = [0,0,1]
 cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
 # cubeStartOrientation = transformations.quaternion_from_euler(math.pi / 2, 0, 0, 'szyx')
 p.setAdditionalSearchPath(str(pathlib.Path(__file__).parent.absolute()))
-# boxId = p.loadURDF("/urdf/humanoid.urdf", cubeStartPos, cubeStartOrientation)
+# boxId = p.loadURDF("/urdf/meshes/stl/robot.urdf", cubeStartPos, cubeStartOrientation)
 boxId = p.loadURDF("/urdf/robot.urdf", cubeStartPos, cubeStartOrientation)
 
 # step through the simluation
