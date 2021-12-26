488a4671d1d590cb1666e6801cbb7ef560ca1c14
sergey-rubtsov <sergey.rubtsov81@gmail.com>
2021-12-26 12:10:13 +0200

    revert debug

--- retarget_motion/retarget_config_od.py
+++ retarget_motion/retarget_config_od.py
@@ -2,9 +2,9 @@ import numpy as np
 
 URDF_FILENAME = "../quadruped_envs/urdf/od.urdf"
 
-REF_POS_SCALE = 0.6
+REF_POS_SCALE = 0.8
 INIT_POS = np.array([0, 0, 0])
-INIT_ROT = np.array([0, 0, 1, 0])
+INIT_ROT = np.array([0, 0, 0, 1.0])
 
 # a - front left
 # b - front right
@@ -17,43 +17,21 @@ INIT_ROT = np.array([0, 0, 1, 0])
 # lleg    2   5   8   11
 
 SIM_LOW_LEG_JOINT_IDS = [
-    5,   # b right hand
-    12,  # d right foot
-    2,   # a left hand
-    8    # c left foot
+    5,   # right hand
+    11,  # right foot
+    2,   # left hand
+    8    # left foot
 ]
 SIM_UP_LEG_JOINT_IDS = [
-    4,   # b right arm
-    10,  # d right knee
-    1,   # a left arm
-    7    # c left knee
+    4,   # right arm
+    10,  # right knee
+    1,   # left arm
+    7    # left knee
 ]
-SIM_HIP_JOINT_IDS = [
-    3,  # b
-    9,  # d
-    0,  # a
-    6   # c
-]
+SIM_HIP_JOINT_IDS = [3, 9, 0, 6]
 
-SIM_ROOT_OFFSET = np.array([0, 0, -0.005])
-SIM_FOOT_OFFSET = [
-    np.array([0.0, -0.1, 0.0]),
-    np.array([0.0, -0.1, 0.0]),
-    np.array([0.0, 0.1, 0.0]),
-    np.array([0.0, 0.1, 0.0])
-]
-SIM_LOW_LEG_OFFSET_LOCAL = [
-    np.array([0.0, 0, 0.0]),
-    np.array([0.0, 0, 0.0]),
-    np.array([0.0, 0, 0.0]),
-    np.array([0.0, 0, 0.0])
-]
-
-SIM_LOW_LEG_ROT = [
-    np.array([0.0, 0, 0.0]),
-    np.array([0.0, 0, 0.0]),
-    np.array([0.0, 0, 0.0]),
-    np.array([0.0, 0, 0.0])
+SIM_ROOT_OFFSET = np.array([0, 0, 0])
+SIM_LOW_LEG_OFFSET_LOCAL = [np.array([0.0, 0.1, 0.0]), np.array([0.0, 0.1, 0.0]), np.array([0.0, -0.1, 0.0]), np.array([0.0, -0.1, 0.0])
 ]
 
 DEFAULT_JOINT_POSE = np.array([0, 0, 1.25,
@@ -61,14 +39,14 @@ DEFAULT_JOINT_POSE = np.array([0, 0, 1.25,
                                0, 0, 1.25,
                                0, 0, 1.25])
 
+# DEFAULT_JOINT_POSE = np.array([0, 0.8, 1.25,
+#                                0, 0.8, 1.25,
+#                                0, 0.8, 1.25,
+#                                0, 0.8, 1.25])
+
 JOINT_DAMPING = [0.1, 0.05, 0.01,
                  0.1, 0.05, 0.01,
                  0.1, 0.05, 0.01,
-                 0.1, 0.05, 0.01,
-                 0.1, 0.05, 0.01,
-                 0.1, 0.05, 0.01,
-                 0.1, 0.05, 0.01,
-                 0.1, 0.05, 0.01
-                 ]
+                 0.1, 0.05, 0.01]
 
-FORWARD_DIR_OFFSET = np.array([0, 0, 0.0])
+FORWARD_DIR_OFFSET = np.array([0, 0, 0.025])
