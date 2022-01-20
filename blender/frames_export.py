fe87e594625368db327a4115c7eed9bf31fed862
sergey-rubtsov <sergey.rubtsov81@gmail.com>
2022-01-20 19:08:00 +0200

    refactor frame export script

--- blender/frames_export.py
+++ blender/frames_export.py
@@ -1,16 +1,22 @@
+# root object should be selected
+
 import bpy
 import numpy as np
 
 FRAME_DURATION = 0.01667
+LABEL = "Export Frames v0.7"
+
+def get_last_frame_number():
+    return bpy.context.scene.frame_end
 
 
-def get_pose_frames(od_armature_name='link_root', frame=1):
+def get_pose_frames(od_armature_name='root', frame=1):
     bone_list = [
         'hip_a', 'u_leg_a', 'l_leg_a',
-        'hip_b', 'u_leg_b', 'l_leg_b',       
-        'hip_c', 'u_leg_c', 'l_leg_c',        
+        'hip_b', 'u_leg_b', 'l_leg_b',
+        'hip_c', 'u_leg_c', 'l_leg_c',
         'hip_d', 'u_leg_d', 'l_leg_d'
-        ]
+    ]
     ob = bpy.context.object
     bpy.context.scene.frame_set(frame)
     od_armature = bpy.context.scene.objects[od_armature_name]
@@ -32,7 +38,7 @@ def get_pose_frames(od_armature_name='link_root', frame=1):
     result.append(mw.to_quaternion().x)
     result.append(mw.to_quaternion().y)
     result.append(mw.to_quaternion().z)
-    result.append(mw.to_quaternion().w) 
+    result.append(mw.to_quaternion().w)
 
     for name in bone_list:
         result.append(ob.convert_space(
@@ -45,11 +51,11 @@ def get_pose_frames(od_armature_name='link_root', frame=1):
     return result
 
 
-def write_frames(context, filepath, frames):
+def write_frames(context, filepath, frames, frame_duration=FRAME_DURATION):
     f = open(filepath, 'w', encoding='utf-8')
     f.write("{\n")
     f.write("\"LoopMode\": \"Wrap\",\n")
-    f.write("\"FrameDuration\": " + str(FRAME_DURATION) + ",\n")
+    f.write("\"FrameDuration\": " + str(frame_duration) + ",\n")
     f.write("\"EnableCycleOffsetPosition\": true,\n")
     f.write("\"EnableCycleOffsetRotation\": true,\n")
     f.write("\n")
@@ -75,46 +81,39 @@ def write_frames(context, filepath, frames):
     f.write("\n]")
     f.write("\n}")
     f.close()
-    
 
     return {'FINISHED'}
 
-
-# ExportHelper is a helper class, defines filename and
-# invoke() function which calls the file selector.
 from bpy_extras.io_utils import ExportHelper
 from bpy.props import StringProperty, BoolProperty, EnumProperty
 from bpy.types import Operator
 
 
 class ExportFrames(Operator, ExportHelper):
-    """This appears in the tooltip of the operator and in the generated docs"""
-    bl_idname = "export_frames.frames_data"  # important since its how bpy.ops.import_test.some_data is constructed
-    bl_label = "Export Frames v0.4"
+    bl_idname = "export_frames.frames_data"
+    bl_label = LABEL
 
-    # ExportHelper mixin class uses this
+    first_frame = bpy.props.IntProperty(name="First frame", default=0)
+    last_frame = bpy.props.IntProperty(name="Last frame", default=get_last_frame_number())
+    frame_duration = bpy.props.FloatProperty(name="Frame duration", default=FRAME_DURATION)
+
     filename_ext = ".txt"
 
     filter_glob = StringProperty(
-            default="*.txt",
-            options={'HIDDEN'},
-            maxlen=255,  # Max internal buffer length, longer would be clamped.
-            )
-
+        default="*.txt",
+        options={'HIDDEN'},
+        maxlen=255,  # Max internal buffer length, longer would be clamped.
+    )
 
     def execute(self, context):
-        first_frame = 90
-        last_frame = 250
-
-        frames = np.zeros([last_frame - first_frame, 19])
-        for i in range(first_frame, last_frame):
-            frames[i - first_frame] = np.asarray(get_pose_frames(frame=i))
-        return write_frames(context, self.filepath, frames)
+        frames = np.zeros([self.last_frame - self.first_frame, 19])
+        for i in range(self.first_frame, self.last_frame):
+            frames[i - self.first_frame] = np.asarray(get_pose_frames(frame=i))
+        return write_frames(context, self.filepath, frames, self.frame_duration)
 
 
-# Only needed if you want to add into a dynamic menu
 def menu_func_export(self, context):
-    self.layout.operator(ExportFrames.bl_idname, text="Frame Export Operator v0.4")
+    self.layout.operator(ExportFrames.bl_idname, text=LABEL)
 
 
 def register():
@@ -138,4 +137,3 @@ def print(data):
 
 if __name__ == "__main__":
     register()
-    
